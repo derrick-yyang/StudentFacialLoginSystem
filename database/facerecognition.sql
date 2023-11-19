95cc2fd35a2a00c150bda1b834009a59d844d3c1
Derrick Yang <57463206+derrick-yyang@users.noreply.github.com>
2023-11-19 14:46:36 +0800

    Derrick/database design (#2)
    * added database design
    
    * create sample data
    
    * added database_utils module for getting useful data from database

--- database/facerecognition.sql
+++ database/facerecognition.sql
@@ -1,52 +1,41 @@
--- phpMyAdmin SQL Dump
--- version 4.6.6deb5
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Feb 17, 2020 at 09:41 PM
--- Server version: 5.7.28-0ubuntu0.18.04.4
--- PHP Version: 7.2.24-0ubuntu0.18.04.1
+CREATE TABLE IF NOT EXISTS Students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(100) NOT NULL,
+    login_time TIMESTAMP NOT NULL,
+    logout_time TIMESTAMP NOT NULL
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE TABLE IF NOT EXISTS Teachers (
+    teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+    teacher_name VARCHAR(100) NOT NULL,
+    teacher_email VARCHAR(100) NOT NULL
+);
 
+CREATE TABLE IF NOT EXISTS Courses (
+    course_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_name VARCHAR(100) NOT NULL,
+    course_description TEXT,
+    teacher_id INT,
+    lecture_notes TEXT,
+    FOREIGN KEY(teacher_id) REFERENCES Teachers(teacher_id)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE IF NOT EXISTS Classes (
+    class_id INT AUTO_INCREMENT PRIMARY KEY,
+    course_id INT,
+    day_of_week VARCHAR(15),
+    start_time TIME,
+    end_time TIME,
+    classroom VARCHAR(50),
+    zoom_link VARCHAR(200),
+    FOREIGN KEY(course_id) REFERENCES Courses(course_id)
+);
 
---
--- Database: `facerecognition`
---
+CREATE TABLE IF NOT EXISTS StudentClass (
+    student_class_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT NOT NULL,
+    class_id INT NOT NULL,
+    FOREIGN KEY(student_id) REFERENCES Students(student_id),
+    FOREIGN KEY(class_id) REFERENCES Classes(class_id)
+);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Student`
---
-DROP TABLE IF EXISTS `Student`;
-
-# Create TABLE 'Student'
-CREATE TABLE `Student` (
-  `student_id` int NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `login_time` time NOT NULL,
-  `login_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-LOCK TABLES `Student` WRITE;
-/*!40000 ALTER TABLE `Student` DISABLE KEYS */;
-INSERT INTO `Student` VALUES (1, "JACK", NOW(), '2021-01-20');
-/*!40000 ALTER TABLE `Student` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Create TABLE 'Course'
-# Create TABLE 'Classroom'
-# Create other TABLE...
-
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
