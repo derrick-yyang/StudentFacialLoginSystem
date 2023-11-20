a9898ade6459ce654aceb532a0e2cda92c1d2cb4
Derrick Yang <yangguangzheng122@gmail.com>
2023-11-20 15:43:42 +0800

    Added renders for course info and course schedule

--- gui/gui_utils.py
+++ gui/gui_utils.py
@@ -9,7 +9,7 @@ class CourseScheduleWindow:
 
         self.schedule = schedule
 
-    def render_schedule(self):
+    def render(self):
         self.create_schedule()
         self.root.mainloop()
     
@@ -71,3 +71,63 @@ class CourseScheduleWindow:
 
         # Position the Course at it's respective day and time
         course_label.grid(row=start_hour, column=day_to_col[day], rowspan=end_hour-start_hour, sticky='nsew')
+
+class CourseInformationWindow:
+    def __init__(self, user, course_details):
+        # Create tkinter window and set the size
+        self.root = tk.Tk()
+        self.root.title("{}'s Upcoming Course Details".format(user))
+        self.root.geometry("1300x800")
+
+        self.course_details = course_details
+
+    def render(self):
+        self.add_course_details()
+        self.root.mainloop()
+
+    def add_course_details(self):
+         # Course name and title
+        title_frame = tk.Frame(self.root)
+        title_frame.pack(side='top', fill='x')
+        title = tk.Label(title_frame, text=self.course_details['name'], font=("Helvetica", 50))
+        title.pack(pady=20)
+
+        # Create a canvas and scrollbar
+        canvas = tk.Canvas(self.root)
+        scrollbar = tk.Scrollbar(self.root, command=canvas.yview)
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        # Create a frame and put it on the canvas
+        frame = tk.Frame(canvas)
+        canvas.pack(side='left', fill='both', expand=True)
+        scrollbar.pack(side='right', fill='y')
+        canvas.create_window((0, 0), window=frame, anchor='nw')
+
+        # Update the scrollregion of the canvas when the size of the frame changes
+        frame.bind('<Configure>', lambda _: canvas.configure(scrollregion=canvas.bbox('all')))
+
+        # Create labels and text boxes for each course information
+        labels = ['Course Description', 'Teacher Name', 'Teacher Email', 'Start Time', 'End Time', 'Classroom', 'Zoom Link', 'Lecture Notes']
+        for i in range(len(labels)):
+            tk.Label(frame, text=labels[i]+":", font=("Helvetica", 20)).grid(row=i, column=0, sticky='', pady=10, padx=40)
+            tk.Label(frame, text=self.course_details[labels[i]], font=("Helvetica", 16)).grid(row=i, column=1, sticky='', pady=10, padx=40)
+
+# # Just for testing ~
+# schedule = {
+#     'Introduction to Databases' : ["9:00", "10:00", 'Tuesday'],
+#     'COMP 3330' : ["4:00", "10:00", 'Sunday']
+# }
+
+# course_details = {}
+# course_details['name'] = "Introduction to Databases"
+# course_details['Course Description'] = "alsjdlasdjfladsfkasdjfksdjlksd"
+# course_details['Teacher Name'] = "Teacher name"
+# course_details['Teacher Email'] = "teachername@gmail.com"
+# course_details['Start Time'] = "14:00:00"
+# course_details['End Time'] = "15:00:00"
+# course_details['Classroom'] = "Room 10023"
+# course_details['Zoom Link'] = "https://zoom.us/j/1234567890"
+# course_details['Lecture Notes'] = "Lecture notes for the course"
+
+# ci = CourseInformationWindow('Derrick', course_details=course_details)
+# ci.render()
